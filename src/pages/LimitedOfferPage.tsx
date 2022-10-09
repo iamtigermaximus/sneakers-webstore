d0f20bf8e1a533d69bfdb6de66be1be47226e694
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-10-10 01:10:06 +0300

    added hero header in limited offer and new arrivals page

--- src/pages/LimitedOfferPage.tsx
+++ src/pages/LimitedOfferPage.tsx
@@ -1,20 +1,4 @@
-import styled from 'styled-components'
-import NikeHeader from '../assets/nike-header.jpg'
-
-const Container = styled.div`
-width: 100vw;
-padding 80px 0 150px;
-height: 100%;
-`
-
-export const Header = styled.div`
-  width: 100%;
-  height: 200px;
-  background-image: url(${NikeHeader});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-`
+import { Container, Header } from '../components/common/Common.styles'
 
 const LimitedOfferPage = () => {
   return (
